28bc89d2b906ad4a7c7ed9375604ceedfbc8e75a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-11 09:14:11 +0200

    Update file_storage.py

--- airbnb/models/engine/file_storage.py
+++ airbnb/models/engine/file_storage.py
@@ -1,8 +1,9 @@
+
+        
 #!/usr/bin/python3
 
 import json
 import os
-
 
 class FileStorage:
     __file_path = "file.json"
@@ -17,13 +18,11 @@ class FileStorage:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
-
-    @classmethod
     def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
         with open(self.__file_path, 'w') as file:
             json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
 
-    @classmethod
     def reload(self):
         """Deserializes the JSON file to __objects (only if the file exists)."""
         try:
@@ -31,8 +30,9 @@ class FileStorage:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    class_instance = globals()[self.__class__.__name__](**value)
+                    class_instance = globals()[class_name](**value)
                     self.__objects[key] = class_instance
         except FileNotFoundError:
             pass
-
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON file: {e}")
