783e96aec679decf1273d3c5cd4eec6c21fa4ef2
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-15 01:55:30 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,9 +14,7 @@ class TestUser(unittest.TestCase):
             password="securepassword",
             first_name="John",
             last_name="Doe",created_at=datetime.datetime.strptime("2022-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),
-            updated_at=datetime.datetime.strptime("2022-01-02 12:00:00", "%Y-%m-%d %H:%M:%S")
-        )
-            
+            updated_at=datetime.datetime.strptime("2022-01-02 12:00:00", "%Y-%m-%d %H:%M:%S"),
             id="unique_id"
         )
 
