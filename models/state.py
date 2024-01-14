b3566a96665b8012fc47de3cbc603fd6a2afb801
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-14 18:55:11 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""
-This modele contains a class that inherits from the BaseModel class
-"""
-
-
+"""This module defines a class State that inherits from BaseModel superclass"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """A class that inherits from the BaseModel class
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if kwargs:
-            self.name = kwargs.get('name', "")
-        else:
-            self.name = ''
+    """State class represents the state"""
+    name = ""
