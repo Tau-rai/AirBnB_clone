722e1bb161a80545efc52e1bb01670defb547bc8
Tau <taumasaire@gmail.com>
2024-01-15 00:09:02 +0200

    updating test user cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 
 import unittest
 from models.user import User
+from datetime import datetime
 
 class TestUser(unittest.TestCase):
 
@@ -40,6 +41,27 @@ class TestUser(unittest.TestCase):
         self.assertEqual(default_user.password, "")
         self.assertEqual(default_user.first_name, "")
         self.assertEqual(default_user.last_name, "")
+
+    def test_check_type(self):
+        self.assertEqual(type(self.sample_user.id), str)
+        self.assertEqual(type(self.sample_user.created_at), datetime)
+        self.assertEqual(type(self.sample_user.updated_at), datetime)
+
+    def test_str_representation(self):
+        expected = "[User] ({}) {}".format(self.sample_user.id, self.sample_user.__dict__)
+        self.assertEqual(str(self.sample_user), expected)
+
+    def test_save(self):
+        self.sample_user.save()
+        self.assertNotEqual(self.sample_user.created_at, self.sample_user.updated_at)
+
+    def test_to_dict(self):
+        user_dict = self.sample_user.to_dict()
+        self.assertEqual(type(user_dict), dict)
+        self.assertTrue('id' in user_dict)
+        self.assertTrue('created_at' in user_dict)
+        self.assertTrue('updated_at' in user_dict)
+        self.assertEqual(user_dict['__class__'], 'User')
 
 if __name__ == '__main__':
     unittest.main()
