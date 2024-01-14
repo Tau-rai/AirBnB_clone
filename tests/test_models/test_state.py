b1225f543110befb4675a0791a5f58ce0249b652
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-14 23:16:47 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -16,7 +16,7 @@ class TestState(unittest.TestCase):
     def test_state_name(self):
         self.assertEqual(self.state.name, "")
 
-        new_name = "California"
+        new_name = "Bulawayo"
         self.state.name = new_name
         self.assertEqual(self.state.name, new_name)
 
@@ -31,28 +31,6 @@ class TestState(unittest.TestCase):
         self.assertIsInstance(str_representation, str)
         self.assertIn(self.state.id, str_representation)
         
-        
-    def test_to_dict_method(self):
-        """
-        Test the to_dict method of State
-        """
-        state = State()
-        state_dict = state.to_dict()
-        self.assertIn("__class__", state_dict)
-        self.assertEqual(state_dict["__class__"], "State")
-        self.assertIn("id", state_dict)
-        self.assertEqual(state_dict["id"], state.id)
-        self.assertIn("created_at", state_dict)
-        self.assertEqual(
-            state_dict["created_at"], state.created_at.isoformat()
-        )
-        self.assertIn("updated_at", state_dict)
-        self.assertEqual(
-            state_dict["updated_at"], state.updated_at.isoformat()
-        )
-        self.assertIn("name", state_dict)
-        self.assertEqual(state_dict["name"], state.name)
-
 
 if __name__ == '__main__':
     unittest.main()
