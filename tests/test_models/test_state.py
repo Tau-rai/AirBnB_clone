287651b845d0eb61525da64e3db0761d0ca79491
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-14 23:03:38 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -30,6 +30,28 @@ class TestState(unittest.TestCase):
         str_representation = str(self.state)
         self.assertIsInstance(str_representation, str)
         self.assertIn(self.state.id, str_representation)
+        
+        
+    def test_to_dict_method(self):
+        """
+        Test the to_dict method of State
+        """
+        state = State()
+        state_dict = state.to_dict()
+        self.assertIn("__class__", state_dict)
+        self.assertEqual(state_dict["__class__"], "State")
+        self.assertIn("id", state_dict)
+        self.assertEqual(state_dict["id"], state.id)
+        self.assertIn("created_at", state_dict)
+        self.assertEqual(
+            state_dict["created_at"], state.created_at.isoformat()
+        )
+        self.assertIn("updated_at", state_dict)
+        self.assertEqual(
+            state_dict["updated_at"], state.updated_at.isoformat()
+        )
+        self.assertIn("name", state_dict)
+        self.assertEqual(state_dict["name"], state.name)
 
 
 if __name__ == '__main__':
