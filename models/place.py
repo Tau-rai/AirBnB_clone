f4f0f67263b2f8c8f3bdbec72b6a589d2334e012
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-14 18:45:59 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,38 +1,35 @@
+
 #!/usr/bin/python3
-"""
-This modele contains a class that inherits from the BaseModel class
-"""
-
-
+"""This module defines the Place class which represents a place and its entities """
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """A class that inherits from the BaseModel class
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if kwargs:
-            self.city_id = kwargs.get('city_id', "")
-            self.user_id = kwargs.get('user_id', "")
-            self.name = kwargs.get('name', "")
-            self.description = kwargs.get('description', "")
-            self.number_rooms = kwargs.get('number_rooms', 0)
-            self.number_bathrooms = kwargs.get('number_bathrooms', 0)
-            self.max_guest = kwargs.get('max_guest', 0)
-            self.price_by_night = kwargs.get('price_by_night', 0)
-            self.latitude = kwargs.get('latitude', 0.0)
-            self.longitude = kwargs.get('longitude', 0.0)
-            self.amenity_ids = kwargs.get('amenity_ids', "")
-        else:
-            self.city_id = ''
-            self.user_id = ''
-            self.name = ''
-            self.description = ''
-            self.number_rooms = 0
-            self.number_bathrooms = 0
-            self.max_guest = 0
-            self.price_by_night = 0
-            self.latitude = 0.0
-            self.longitude = 0.0
-            self.amenity_ids = ''
+    Place class represents a specific place.
+
+    Attributes:
+        city_id: string - empty string: it will be the City.id
+        user_id: string - empty string: it will be the User.id
+        name: string - empty string
+        description: string - empty string
+        number_rooms: integer - 0
+        number_bathrooms: integer - 0
+        max_guest: integer - 0
+        price_by_night: integer - 0
+        latitude: float - 0.0
+        longitude: float - 0.0
+        amenity_ids: list of string - empty list:
+        it will be the list of Amenity.id later
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
