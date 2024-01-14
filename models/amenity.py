09bb404d1bf588c5d354ea8f25de4dac4d43b598
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-14 18:51:14 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -10,9 +10,4 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """A class that inherits from the BaseModel class
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if kwargs:
-            self.name = kwargs.get('name', "")
-        else:
-            self.name = ''
+    self.name = ''
